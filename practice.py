2acae0beda1ecaa5db1230fa756406127c3c95dd
Luong Vy <luongtuanvy04@gmail.com>
2024-07-21 00:27:24 +0700

    add completed task

--- practice.py
+++ practice.py
@@ -82,6 +82,7 @@ print(full_sorted)
 
 # TASK 6
 
+"""
 # string to experiment with: place
 place = "poolhouse"
 
@@ -95,3 +96,141 @@ print(place_up)
 
 # Print out the number of o's in place
 print(place.count("o"))
+"""
+
+# TASK 7
+
+"""
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+"""
+
+# TASK 8
+
+"""
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+"""
+
+# TASK 9
+
+"""
+import numpy as np
+
+fam = ["liz", 1.73, "emma", 1.68, "mom", 1.71, "dad", 1.89]
+
+fam_ext = fam + ["me", 1.79]
+
+print(str(len(fam_ext)) + " elements in fam_ext")
+
+np_fam = np.array(fam_ext)
+
+"""
+
+# TASK 10
+
+"""
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * 0.43 * math.pi
+
+# Calculate A
+A = math.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+"""
+
+# TASK 11
+
+"""
+# Import pi function of math package
+from math import pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+"""
+
+# TASK 12
+
+"""
+import numpy as np
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
+
+bmi = np_weight / np_height ** 2
+
+print(bmi)
+print(bmi > 21)
+"""
+
+# TASK 13
+
+"""
+# Import the numpy package as np
+import numpy as np
+
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+"""
+
+# TASK 14
+
+"""
+# Import numpy, Create height_in
+import numpy as np
+height_in = [74, 74, 72, 75, 76, 73]
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+"""
+
+# TASK 15
+
+import numpy as np 
+
+np_test = np.array([True, 1, 2]) + np.array([3, 4, False])
+print(np_test)
