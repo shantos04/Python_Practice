fbad113e975ce75942ef7bacf2c6c2855341852a
Luong Vy <luongtuanvy04@gmail.com>
2024-07-20 13:28:28 +0700

    add file practice

---
+++ practice.py
@@ -0,0 +1,10 @@
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list
+del areas[-4:-2]
+
+# Print the updated list
+print(areas)
