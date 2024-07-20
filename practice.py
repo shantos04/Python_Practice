2f6bd4df15cb8ef38f4b5d26ca06b86ffcf32949
Luong Vy <luongtuanvy04@gmail.com>
2024-07-20 20:04:44 +0700

    add completed tasks

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+# TASK 1
 """" PRACTICE DELETE IN PYTHON
 areas = ["hallway", 11.25, "kitchen", 18.0,
         "chill zone", 20.0, "bedroom", 10.75,
@@ -10,6 +11,8 @@ del areas[-4:-2]
 # Print the updated list
 print(areas)
 """
+
+# TASK 2
 
 """ Currently, the first element in the areas_copy list is changed and the areas list is printed out.
  If you hit the run code button you'll see that, although you've changed areas_copy,
@@ -27,6 +30,9 @@ areas_copy[0] = 5.0
 print(areas)
 """
 
+#TASK 3
+
+"""
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
@@ -38,3 +44,54 @@ areas_copy[0] = 5.0
 
 # Print areas
 print(areas)
+"""
+
+# TASK 4
+
+"""
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+"""
+
+# TASK 5
+
+"""
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse = True)
+
+# Print out full_sorted
+print(full_sorted)
+"""
+
+# TASK 6
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+
+# Print out the number of o's in place
+print(place.count("o"))
