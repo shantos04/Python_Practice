bd72323e1f8e4d466b55d6e721e997fa20cad373
Luong Vy <luongtuanvy04@gmail.com>
2024-07-21 23:50:07 +0700

    Add new practice

---
+++ practice2.py
@@ -0,0 +1,21 @@
+# TASK 1
+
+"""
+# Create the playlist variable
+playlist = [1, "Blinding Lights", 2, "One Dance", 3, "Uptown Funk"]
+
+# Print the list
+print(playlist)
+
+# Print all songs in the playlist
+print(playlist[1::3])
+
+# Print the song by Coldplay
+print(playlist["Coldplay"])
+
+# Add a new song
+playlist["Usher"] = "Yeah!"
+
+# Print the songs in the playlist
+print(playlist.values())
+"""
