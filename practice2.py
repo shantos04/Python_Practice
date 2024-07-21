011a7ad6c0b9e48926c4d08212046504c16aea9a
Luong Vy <luongtuanvy04@gmail.com>
2024-07-22 00:10:12 +0700

    Add completed tasks

--- practice2.py
+++ practice2.py
@@ -19,3 +19,23 @@ playlist["Usher"] = "Yeah!"
 # Print the songs in the playlist
 print(playlist.values())
 """
+
+# TASK 2
+
+# Create a tuple
+q3_financials = (325780, 1041, 4271599)
+
+# Print the tuple
+print(q3_financials)
+
+hip_hop = ["Drake", "JAY-Z", "50 Cent", "Drake"]
+
+# Create a set
+indie_set = {"Kings of Leon", "Arctic Monkeys", "Stereophonics"}
+
+# Convert hip_hop to a set
+hip_hop_set = set(hip_hop)
+
+# Print the indie and hip_hop sets
+print(indie_set)
+print(hip_hop_set)
