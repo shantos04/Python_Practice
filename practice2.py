38868f8482d3ed683267889cec856594a7483d96
Luong Vy <luongtuanvy04@gmail.com>
2024-07-22 12:38:19 +0700

    Add completed tasks

--- practice2.py
+++ practice2.py
@@ -22,6 +22,7 @@ print(playlist.values())
 
 # TASK 2
 
+"""
 # Create a tuple
 q3_financials = (325780, 1041, 4271599)
 
@@ -39,3 +40,42 @@ hip_hop_set = set(hip_hop)
 # Print the indie and hip_hop sets
 print(indie_set)
 print(hip_hop_set)
+"""
+
+# TASK 3
+
+"""
+# Check if September inflation is less than August inflation
+if inflation_september < inflation_august:
+  print("Inflation decreased")
+
+# Check if September inflation is more than August inflation
+elif inflation_september > inflation_august:
+  print("Inflation increased")
+  
+# Confirm that they are equal
+else:
+  print("Inflation remained stable")
+"""
+
+# TASK 4
+
+"""
+# Check the number of beds
+if num_beds < min_num_beds:
+  print("Insufficient bedrooms")
+
+# Check square feet
+elif sq_foot <= min_sq_foot:
+  print("Too small")
+
+  
+# Check the rent
+elif rent > max_rent:
+  print("Too expensive")
+
+  
+# If all conditions met
+else:
+  print("This looks promising!")
+"""
