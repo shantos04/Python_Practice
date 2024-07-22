ff10c17a389952a3c4c0a0cbe5fd71b1a9ebfeb0
Luong Vy <luongtuanvy04@gmail.com>
2024-07-22 15:21:39 +0700

    add completed tasks

--- practice2.py
+++ practice2.py
@@ -79,3 +79,98 @@ elif rent > max_rent:
 else:
   print("This looks promising!")
 """
+
+# TASK 5
+
+"""
+# Create the tickets_sold variable
+tickets_sold = 0
+
+# Create the max_capacity variable
+max_capacity = 30
+
+# Loop through a range up to and including max_capacity's value
+for i in range(1, max_capacity+1):
+  
+  # Add one to tickets_sold in each iteration
+  tickets_sold += 1
+  
+print("Sold out:", tickets_sold, "tickets sold!")
+"""
+
+# TASK 6
+
+"""
+# Loop through the dictionary's keys and values
+for key, value in courses.items():
+  
+  # Check if the value is "AI"
+  if value == "AI":
+    print(key, "is an AI course")
+  
+  # Check if the value is "Programming"
+  elif value == "Programming":
+    print(key, "is a Programming course")
+  
+  # Otherwise, print that it is a "Data Engineering" course
+  else:
+    print(key, "is a Data Engineering course")
+"""
+
+# TASK 7
+
+"""
+tickets_sold = 0
+max_capacity = 50
+
+# Create a while loop
+while tickets_sold < max_capacity:
+  
+  # Add one to tickets_sold in each iteration
+  tickets_sold += 1
+
+# Print the number of tickets sold
+print("Sold out:", tickets_sold, "tickets sold!")
+"""
+
+# TASK 8
+
+"""
+release_date = 26
+current_date = 19
+
+# Create a condition where current_date is less than or equal to the release_date
+while current_date <= release_date:
+  
+  # Increment current_date by one
+  current_date += 1
+  
+  # Promote purchases
+  if current_date < 21:
+    print("Purchase before the 21st for early access")
+  
+  # Check if the date is less than or equal to the 25th
+  elif current_date <= 25:
+    print("Coming soon!")
+  else:
+    print("Available now!")
+"""
+
+# TASK 9
+
+"""
+# Create an empty list
+authors_below_twenty_five = []
+
+# Loop through the authors dictionary
+for key, value in authors.items():
+  
+  # Check for values less than 25
+  if value < 25:
+    
+    # Append the author to the list
+    authors_below_twenty_five.append(key)
+    
+print(authors_below_twenty_five)
+"""
+
