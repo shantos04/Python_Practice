368cdc6742f9a185431981f1226de8c38f108993
Luong Vy <luongtuanvy04@gmail.com>
2024-07-25 19:56:46 +0700

    Add completed task

--- practice2.py
+++ practice2.py
@@ -174,3 +174,15 @@ for key, value in authors.items():
 print(authors_below_twenty_five)
 """
 
+# TASK 10
+
+
+# Create list comprehension: squares
+squares = [i**2 for i in range(0,10)]
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
