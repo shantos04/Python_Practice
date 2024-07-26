100bbd9db498d212f34903c2384f0b8935f122bc
Luong Vy <luongtuanvy04@gmail.com>
2024-07-26 22:51:18 +0700

    Add completed task

--- intermediate_python.py
+++ intermediate_python.py
@@ -262,153 +262,17 @@ snake_case("User Name 187")
 # TASK 16
 
 """
-# Create a list of strings: flash
-flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+# Create an empty list
+authors_below_twenty_five = []
 
-# Print each list item in flash using a for loop
-for person in flash:
-    print(person)
-
-
-# Create an iterator for flash: superhero
-superhero = iter(flash)
-
-# Print each item from the iterator
-print(next(superhero))
-print(next(superhero))
-print(next(superhero))
-print(next(superhero))
-"""
-
-# TASK 17
-
-"""
-# Create an iterator for range(3): small_value
-small_value = iter(range(3))
-
-# Print the values in small_value
-print(next(small_value))
-print(next(small_value))
-print(next(small_value))
-
-# Loop over range(3) and print the values
-for num in range(3):
-    print(num)
-
-
-# Create an iterator for range(10 ** 100): googol
-googol = iter(range(10 ** 100))
-
-# Print the first 5 values from googol
-print(next(googol))
-print(next(googol))
-print(next(googol))
-print(next(googol))
-print(next(googol))
-"""
-
-# TASK 18
-
-"""
-# Create a range object: values
-values = range(10, 21)
-
-# Print the range object
-print(values)
-
-# Create a list of integers: values_list
-values_list = list(values)
-
-# Print values_list
-print(values_list)
-
-# Get the sum of values: values_sum
-values_sum = sum(values)
-
-# Print values_sum
-print(values_sum)
-"""
-
-# TASK 19
-
-"""
-# Create a list of strings: mutants
-mutants = ['charles xavier', 
-            'bobby drake', 
-            'kurt wagner', 
-            'max eisenhardt', 
-            'kitty pryde']
-
-# Create a list of tuples: mutant_list
-mutant_list = list(enumerate(tuple(mutants)))
-
-# Print the list of tuples
-print(mutant_list)
-
-# Unpack and print the tuple pairs
-for index1, value1 in enumerate(mutants):
-    print(index1, value1)
-
-# Change the start index
-for index2, value2 in enumerate(mutants, start = 1):
-    print(index2, value2)
-"""
-
-# TASK 20
-
-"""
-mutants = ['charles xavier', 
-            'bobby drake', 
-            'kurt wagner', 
-            'max eisenhardt', 
-            'kitty pryde']
-
-aliases = ['prof x', 'iceman', 'nightcrawler', 'magneto', 'shadowcat']
-
-powers = ['telepathy', 'thermokinesis', 'teleportation', 'magnetokinesis', 'intangibility']
-# Create a list of tuples: mutant_data
-mutant_data = list(zip(mutants, aliases, powers))
-
-# Print the list of tuples
-print(mutant_data)
-
-# Create a zip object using the three lists: mutant_zip
-mutant_zip = zip(mutants, aliases, powers)
-
-# Print the zip object
-print(mutant_zip)
-
-# Unpack the zip object and print the tuple values
-for value1, value2, value3 in mutant_zip:
-    print(value1, value2, value3)
-"""
-
-# TASK 21
-
-"""
-mutants = ['charles xavier', 
-            'bobby drake', 
-            'kurt wagner', 
-            'max eisenhardt', 
-            'kitty pryde']
-
-aliases = ['prof x', 'iceman', 'nightcrawler', 'magneto', 'shadowcat']
-
-powers = ['telepathy', 'thermokinesis', 'teleportation', 'magnetokinesis', 'intangibility']
-
-# Create a zip object from mutants and powers: z1
-z1 = zip(mutants, powers)
-
-# Print the tuples in z1 by unpacking with *
-print(*z1)
-
-# Re-create a zip object from mutants and powers: z1
-z1 = zip(mutants, powers)
-
-# 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
-result1, result2 = zip(*z1)
-
-# Check if unpacked tuples are equivalent to original tuples
-print(result1 == tuple(mutants))
-print(result2 == tuple(powers))
+# Loop through the authors dictionary
+for key, value in authors.items():
+  
+  # Check for values less than 25
+  if value < 25:
+    
+    # Append the author to the list
+    authors_below_twenty_five.append(key)
+    
+print(authors_below_twenty_five)
 """
