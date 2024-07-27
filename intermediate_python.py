08e1ddc8deea17d97c8e102410bbf0ea6ab46cae
Luong Vy <luongtuanvy04@gmail.com>
2024-07-27 21:01:07 +0700

    Add completed task

--- intermediate_python.py
+++ intermediate_python.py
@@ -259,20 +259,3 @@ def snake_case(text):
 snake_case("User Name 187")
 """
 
-# TASK 16
-
-"""
-# Create an empty list
-authors_below_twenty_five = []
-
-# Loop through the authors dictionary
-for key, value in authors.items():
-  
-  # Check for values less than 25
-  if value < 25:
-    
-    # Append the author to the list
-    authors_below_twenty_five.append(key)
-    
-print(authors_below_twenty_five)
-"""
