d9e62cfcbeabb382219f26ae1a92c1f3ce51b9f2
Luong Vy <luongtuanvy04@gmail.com>
2024-08-07 20:39:22 +0700

    Add completed task

--- data_types_in_python.py
+++ data_types_in_python.py
@@ -290,3 +290,36 @@ if penguin_305_details["tracked"]:
     # Print the tracked key and tracked_is_true
     print(f"{penguin_305_details['tracked']}: {tracked_is_true}")
 """
+
+# TASK 21
+
+"""
+# Use a list comprehension to iterate over each penguin in penguins saved as female_species_list
+# If the the sex of the penguin is 'FEMALE', return the species value
+female_species_list = [penguin["species"] for penguin in penguins if penguin["sex"] == 'FEMALE']
+
+# Create a set using the female_species_list as female_penguin_species
+female_penguin_species = set(female_species_list)
+
+# Find the difference between female_penguin_species and male_penguin_species. Store the result as differences
+differences = female_penguin_species.difference(male_penguin_species)
+
+# Print the differences
+print(differences)
+"""
+
+# TASK 22
+
+"""
+# Find the union: all_species
+all_species = female_penguin_species.union(male_penguin_species)
+
+# Print the count of names in all_species
+print(len(all_species))
+
+# Find the intersection: overlapping_species
+overlapping_species = female_penguin_species.intersection(male_penguin_species)
+
+# Print the count of species in overlapping_species
+print(len(overlapping_species))
+"""
