1be12ccc097ecbf79d66c2dfc797645eb2420dab
Luong Vy <luongtuanvy04@gmail.com>
2024-08-10 22:33:10 +0700

    Add completed task

---
+++ importing_data_in_python.py
@@ -0,0 +1,20 @@
+# TASK 1
+
+"""
+# Open a file as read-only and bind it to file
+with open('moby_dick.txt', 'r') as file:
+  	# Print it
+    print(file.read())
+    file.close()
+"""
+
+# TASK 2
+
+"""
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+    file.close()
+"""
