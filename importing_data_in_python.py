a6bdb551e07bc5e9ae0ffbf5acd4b2ce72c4541b
Luong Vy <luongtuanvy04@gmail.com>
2024-08-11 20:40:19 +0700

    Add completed task and file txt for task 5

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -18,3 +18,71 @@ with open('moby_dick.txt') as file:
     print(file.readline())
     file.close()
 """
+
+# TASK 3
+
+"""
+# Import packages
+import numpy as np
+
+# Assign filename to variable: file
+file = 'digits.csv'
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=',')
+
+# Print datatype of digits
+print(type(digits))
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+
+"""
+
+# TASK 4
+
+"""
+# Import numpy
+import numpy as np
+
+# Assign the filename: file
+file = 'digits_header.txt'
+
+# Load the data: data
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[0, 2])
+
+# Print data
+print(data)
+
+"""
+
+# TASK 5
+
+"""
+# Assign filename: file
+file = 'seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
+"""
