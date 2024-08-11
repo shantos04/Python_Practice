ad10b39aa8be50de95a0fa130efd49d4bf2dc42e
Luong Vy <luongtuanvy04@gmail.com>
2024-08-11 20:54:42 +0700

    Add completed task

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -86,3 +86,35 @@ plt.ylabel('percentage of larvae')
 plt.show()
 
 """
+
+# TASK 6
+
+"""
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+# View the head of the DataFrame
+print(df.head())    
+"""
+
+# TASK 7
+
+"""
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows = 5, header = None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.to_numpy()
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+"""
