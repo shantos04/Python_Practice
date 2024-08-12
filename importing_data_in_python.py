ea55fbb15a9a6628152a5502ea3872395eca1169
Luong Vy <luongtuanvy04@gmail.com>
2024-08-12 20:49:09 +0700

    Add completed task

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -141,3 +141,53 @@ plt.ylabel('count')
 plt.show()
 
 """
+
+# TASK 9 
+
+"""
+# Import pickle package
+import pickle 
+
+# Open pickle file and load data: d
+with open('data.pkl', 'rb') as file:
+    d = pickle.load(file)
+
+# Print d
+print(d)
+
+# Print datatype of d
+print(type(d))
+"""
+
+# TASK 10
+
+"""
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename: file
+file = 'battledeath.xlsx'
+
+# Load spreadsheet: xls
+xls = pd.ExcelFile(file)
+
+# Print sheet names
+print(xls.sheet_names)
+
+"""
+
+# TASK 11
+
+"""
+# Load a sheet into a DataFrame by name: df1
+df1 = xls.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+"""
