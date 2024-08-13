ca383e873fc8f66c81edb31447e37cf710148fdb
Luong Vy <luongtuanvy04@gmail.com>
2024-08-13 21:37:17 +0700

    Add completed task

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -174,7 +174,7 @@ xls = pd.ExcelFile(file)
 # Print sheet names
 print(xls.sheet_names)
 
-"""
+""" 
 
 # TASK 11
 
@@ -187,6 +187,22 @@ print(df1.head())
 
 # Load a sheet into a DataFrame by index: df2
 df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+"""
+
+# TASK 12
+
+"""
+# Parse the first sheet and rename the columns: df1
+df1 = xls.parse(0, skiprows=[0], names=['Country', 'AAM due to War (2002)'])
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xls.parse(1, usecols=[0], skiprows=[0], names=['Country'])
 
 # Print the head of the DataFrame df2
 print(df2.head())
