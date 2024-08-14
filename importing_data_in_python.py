18fbfe3b9dc473f08a534cea0f8329c19e06e4a7
Luong Vy <luongtuanvy04@gmail.com>
2024-08-14 20:14:11 +0700

    Add completed task

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -207,3 +207,41 @@ df2 = xls.parse(1, usecols=[0], skiprows=[0], names=['Country'])
 # Print the head of the DataFrame df2
 print(df2.head())
 """
+
+# TASK 13
+
+"""
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+"""
+
+# TASK 14
+
+"""
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+"""
