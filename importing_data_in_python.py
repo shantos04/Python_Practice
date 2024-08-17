6ec4c9e25b2b356d3ac9e7ee2ad64b39ead66c2e
Luong Vy <luongtuanvy04@gmail.com>
2024-08-17 20:14:11 +0700

    Add completed task

--- importing_data_in_python.py
+++ importing_data_in_python.py
@@ -433,3 +433,70 @@ with engine.connect() as con:
 # Print head of DataFrame
 print(df.head())
 """
+
+# TASK 25
+
+"""
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Album", engine)
+
+# Print head of DataFrame
+print(df.head())
+
+# Open engine in context manager and store query result in df1
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+
+# Confirm that both methods yield the same result
+print(df.equals(df1))
+"""
+
+# TASK 26
+
+"""
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate", engine)
+
+# Print head of DataFrame
+print(df.head())
+"""
+
+# TASK 27
+
+"""
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist ON Album.ArtistId = Artist.ArtistId")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+"""
+
+# TASK 28
+
+"""
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM PlaylistTrack INNER JOIN Track ON PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000", engine)
+
+# Print head of DataFrame
+print(df.head())
+"""
