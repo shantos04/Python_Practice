3847649b45e3691a7c22de35524c1618478811de
Luong Vy <luongtuanvy04@gmail.com>
2024-08-20 19:39:08 +0700

    Add completed task

--- intermediate_importing_data_in_python.py
+++ intermediate_importing_data_in_python.py
@@ -59,3 +59,69 @@ print(xls.keys())
 # Print the head of the first sheet (using its name, NOT its index)
 print(xls['1700'].head())
 """
+
+# TASK 4
+
+"""
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request: request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Print the datatype of response
+print(type(response))
+
+# Be polite and close the response!
+response.close()
+"""
+
+# TASK 5
+
+"""
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+"""
+
+# TASK 6
+
+"""
+# Import package
+import requests
+
+# Specify the url: url
+url = "http://www.datacamp.com/teach/documentation"
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
+"""
