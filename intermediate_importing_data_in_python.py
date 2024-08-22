6e574430fea2074ea98fee1021373c724c11d834
Luong Vy <luongtuanvy04@gmail.com>
2024-08-22 21:44:04 +0700

    Add completed task

--- intermediate_importing_data_in_python.py
+++ intermediate_importing_data_in_python.py
@@ -227,3 +227,37 @@ with open("a_movie.json") as json_file:
 for k in json_data.keys():
     print(k + ': ', json_data[k])
 """
+
+# TASK 11
+
+"""
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=the+social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
+"""
+
+# TASK 12
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
