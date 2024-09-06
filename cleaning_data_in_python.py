7749e173afa2f47346f8ff4b62f2d248cf9a3e8d
Luong Vy <luongtuanvy04@gmail.com>
2024-09-06 22:45:17 +0700

    Add new task

--- cleaning_data_in_python.py
+++ cleaning_data_in_python.py
@@ -33,3 +33,38 @@ assert ride_sharing['duration_time'].dtype == 'int'
 print(ride_sharing[['duration','duration_trim','duration_time']])
 print(ride_sharing['duration_time'].mean())
 """
+
+# TASK 3
+
+"""
+# Convert tire_sizes to integer
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('int')
+
+# Set all values above 27 to 27
+ride_sharing.loc[ride_sharing['tire_sizes'] > 27, 'tire_sizes'] = 27
+
+# Reconvert tire_sizes back to categorical
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('category')
+
+# Print tire size description
+print(ride_sharing['tire_sizes'].describe())
+"""
+
+# TASK 4
+
+"""
+# Convert ride_date to date
+ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date']).dt.date
+
+# Save today's date
+today = dt.date.today()
+
+# Set all in the future to today's date
+ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
+
+# Print maximum of ride_dt column
+print(ride_sharing['ride_dt'].max())
+"""
+
+# TASK 5
+
